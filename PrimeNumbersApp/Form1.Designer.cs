5650496eea9237d1b30c55db1c0882ca59429043
nickiis <nickii.sonnenberg@gmail.com>
2016-09-20 07:14:00 -0500

    Prime Number application started 07 2015.

---
+++ PrimeNumbersApp/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PrimeNumbersApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComputePrimesButton = new System.Windows.Forms.Button();
+            this.DisplayPrimeNumberTextbox = new System.Windows.Forms.TextBox();
+            this.PrintButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ComputePrimesButton
+            // 
+            this.ComputePrimesButton.Location = new System.Drawing.Point(12, 12);
+            this.ComputePrimesButton.Name = "ComputePrimesButton";
+            this.ComputePrimesButton.Size = new System.Drawing.Size(121, 23);
+            this.ComputePrimesButton.TabIndex = 0;
+            this.ComputePrimesButton.Text = "Compute Primes:";
+            this.ComputePrimesButton.UseVisualStyleBackColor = true;
+            this.ComputePrimesButton.Click += new System.EventHandler(this.ComputePrimesButton_Click);
+            // 
+            // DisplayPrimeNumberTextbox
+            // 
+            this.DisplayPrimeNumberTextbox.Location = new System.Drawing.Point(139, 15);
+            this.DisplayPrimeNumberTextbox.Name = "DisplayPrimeNumberTextbox";
+            this.DisplayPrimeNumberTextbox.ReadOnly = true;
+            this.DisplayPrimeNumberTextbox.Size = new System.Drawing.Size(133, 20);
+            this.DisplayPrimeNumberTextbox.TabIndex = 1;
+            // 
+            // PrintButton
+            // 
+            this.PrintButton.Enabled = false;
+            this.PrintButton.Location = new System.Drawing.Point(139, 68);
+            this.PrintButton.Name = "PrintButton";
+            this.PrintButton.Size = new System.Drawing.Size(75, 23);
+            this.PrintButton.TabIndex = 2;
+            this.PrintButton.Text = "Print to XPS";
+            this.PrintButton.UseVisualStyleBackColor = true;
+            this.PrintButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.PrintButton);
+            this.Controls.Add(this.DisplayPrimeNumberTextbox);
+            this.Controls.Add(this.ComputePrimesButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ComputePrimesButton;
+        private System.Windows.Forms.TextBox DisplayPrimeNumberTextbox;
+        private System.Windows.Forms.Button PrintButton;
+    }
+}
+
---
+++ PrimeNumbersApp/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace PrimeNumbersApp
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer _computeFor60Seconds = new System.Timers.Timer(60000);
+
+        private bool _continue = false;
+
+        private Font _font;
+        private Brush _brush;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static bool IsPrime(int number)
+        {
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            if (number == 1) return false;
+            if (number == 2) return true;
+
+            for (int i = 2; i <= boundary; ++i)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        private void ComputePrimesButton_Click(object sender, EventArgs e)
+        {
+            ComputePrimesButton.Enabled = false;
+            _computeFor60Seconds.Elapsed += OnEndTimer;
+            _continue = _computeFor60Seconds.Enabled = true;
+            int start = 2;
+            while (_continue)
+            {
+                if (IsPrime(start))
+                {
+                    DisplayPrimeNumberTextbox.Text = start.ToString();
+                }
+                start++;
+            }
+            PrintButton.Enabled = true;
+        }
+
+        private void OnEndTimer(object sender, ElapsedEventArgs e)
+        {
+            _continue = _computeFor60Seconds.Enabled = false;
+        }
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDlg = new PrintDialog();
+            printDlg.Document = new System.Drawing.Printing.PrintDocument();
+            printDlg.Document.PrinterSettings.PrinterName = "Microsoft XPS Document Writer";
+            printDlg.Document.PrinterSettings.PrintToFile = true; 
+            if (printDlg.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            _font = new System.Drawing.Font("Arial", 10);
+            _brush = new SolidBrush(Color.Black);
+            printDlg.Document.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(PrintPrime_PrintPage);
+
+            printDlg.Document.Print();
+        }
+
+        private void PrintPrime_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString("Nickii Sonnenberg: " + DisplayPrimeNumberTextbox.Text, _font, _brush, 0, 0);
+        }
+    }
+}
